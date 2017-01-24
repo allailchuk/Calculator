905f02f80c319dde702acf956a73f2eb3726afbf
Alla Ilchuk <alla.ilchuk@gmail.com>
2017-01-23 23:20:42 -0800

    added new functionality - Reset and Delete buttons

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
     var firstValue = "";
     var secondValue = "";
     var operator;
-    var newValue;
+    var decimal;
+    var editedValue;
             
         function addValue(value) {
-            // checking if operator was not set
+                // checking if operator was not set
             if (typeof operator !== "string"){
+                //accumulating values (numbers clicked) into firstValue and converting it into a string
                 firstValue += value.toString();
+                // showing firstValue on the calculator display
                 document.getElementsByClassName("display")[0].innerHTML = firstValue;
 
             } else {
@@ -15,27 +18,23 @@
             }
         }
             
-
         function addOperator(o) {
-                 
             if (firstValue !== '' && secondValue !=='' && typeof operator === "string"){
                 total();
             }
-            
-            operator = o;
-                
+            operator = o;          
         }
 
         function total() { 
-                            
+                          
             if (operator == "+") {
                 var total = +firstValue + +secondValue;
             } else if (operator == "-") {
-                var total = +firstValue - +secondValue;
+                var total = +firstValue - +secondValue;               
             } else if (operator == "*") {
-                var total = +firstValue * +secondValue;
+                var total = +firstValue * +secondValue;                
             } else if (operator == "/") {
-                var total = +firstValue / +secondValue;
+                var total = +firstValue / +secondValue;                
             }  
             
             document.getElementsByClassName("display")[0].innerHTML = total;
@@ -43,5 +42,62 @@
             secondValue = '';
             operator = undefined;                
         }
+
+        function resetValue(){
+            document.getElementsByClassName("display")[0].innerHTML = '';
+            firstValue = '';
+            secondValue = '';
+        }
+
+        // function deleteSymbol(){
+        //     var editedValue = document.getElementsByClassName("display")[0].innerHTML.slice(0, -1);
+        //     document.getElementsByClassName("display")[0].innerHTML= editedValue;
+        //     if (operator !== "string"){ 
+        //         firstValue = editedValue;
+        //     } else if (operator && firstValue){
+        //         secondValue = editedValue;
+        //     }
+
+        // }
+
+        function deleteSymbol (){
+            debugger;
+            if (typeof operator !== "string") {
+                document.getElementsByClassName("display")[0].innerHTML = firstValue = firstValue.slice(0, -1);
+            } else{
+                document.getElementsByClassName("display")[0].innerHTML = secondValue = secondValue.slice(0, -1);
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
