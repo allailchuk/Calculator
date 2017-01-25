8e262df4f150a298fa106b32463cf94d7e808fd0
Alla Ilchuk <alla.ilchuk@gmail.com>
2017-01-25 15:58:02 -0800

    Added square root functionality, keyboard (keypress/keydown functionality

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
     var operator;
     var editedValue;
     var memory = 0;
-  
+    var root;
+    
    
         function tablo(value) {
            document.getElementsByClassName("display")[0].innerHTML = value; 
@@ -46,13 +47,13 @@
             operator = undefined;                
         }
 
-        function resetValue(){
+        function resetValue() {
             tablo('');
             firstValue = '';
             secondValue = '';
         }
 
-        function deleteSymbol(){
+        function deleteSymbol() {
             if (!operator) {
                 firstValue = firstValue.slice(0, -1);
                 tablo(firstValue);
@@ -62,7 +63,7 @@
             }
         }
 
-        function addToMemory(){
+        function addToMemory() {
             if (!operator) {
                 memory += +firstValue;
             } else {
@@ -74,21 +75,75 @@
             operator = undefined; 
         }
 
-        function showMemory(){
+        function showMemory() {
             if (!operator) {
                 firstValue = memory;
             } else {
                 secondValue = memory;
             } 
-              
+
             tablo(memory);
         }
 
-        function clearMemory(){
+        function clearMemory() {
             memory = 0;     
         }
 
+        function squareRoot() {
+            if (!operator) {
+                root = Math.sqrt(firstValue);
+                firstValue = root;
+            } else {
+                root = Math.sqrt(secondValue);
+                secondValue = root;
+            }
 
+            tablo(root);
+        } 
+        
+        document.addEventListener("keypress", keyboardPress);
+        
+        function keyboardPress(pressed) {
+            if (pressed.keyCode == '49') {
+                addValue(pressed.key);
+            } else if (pressed.keyCode == '50') {
+                addValue(pressed.key);
+            } else if (pressed.keyCode == '51') {
+                addValue(pressed.key);
+            } else if (pressed.keyCode == '52') {
+                addValue(pressed.key);
+            } else if (pressed.keyCode == '53') {
+                addValue(pressed.key);
+            } else if (pressed.keyCode == '54') {
+                addValue(pressed.key);
+            } else if (pressed.keyCode == '55') {
+                addValue(pressed.key);
+            } else if (pressed.keyCode == '56') {
+                addValue(pressed.key);
+            } else if (pressed.keyCode == '57') {
+                addValue(pressed.key);
+            } else if (pressed.keyCode == '48') {
+                addValue(pressed.key);
+            } else if (pressed.key == "+") {
+                addOperator(pressed.key);
+            } else if (pressed.key == "-") {
+                addOperator(pressed.key);
+            } else if (pressed.key == "*") {
+                addOperator(pressed.key);
+            } else if (pressed.key == "/") {
+                addOperator(pressed.key);
+            } else if (pressed.key == "=") {
+                total();
+            } 
+        }
+
+        document.addEventListener("keydown", deleteOnKeyboard);
+
+        function deleteOnKeyboard (pressed) {
+            if (pressed.keyCode == '8') {
+                resetValue();
+            }
+        }
 
 
 
